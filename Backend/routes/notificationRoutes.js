f5872329e26db7408ba57963270bbe8d679359ab
Bhagya Subhasinghe <bnsubhasinghe@gmail.com>
2025-04-26 20:00:59 +0530

    First commit

--- Backend/routes/notificationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendSMS } = require('../controllers/notificationController');
-
-router.post('/sms', sendSMS);
-
-module.exports = router;
